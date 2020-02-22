7a8c1af94a91bfe241f0646b367529f3e5470541
Jon <chipdale0119@gmail.com>
2020-02-22 09:32:53 +0000

    remove commentout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		# strong parametersを設定し、idnameを許可
-		# devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :idname, :password, :password_confirmation])
-		# devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :idname, :password, :password_confirmation])
 		devise_parameter_sanitizer.permit(:sign_in, keys: [:idname])
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :idname])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :idname])
