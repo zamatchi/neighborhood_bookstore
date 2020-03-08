3c1201462afacd389e126ddd2aa0892a48a8eceb
Jon <chipdale0119@gmail.com>
2020-03-08 10:42:11 +0000

    add admin_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# (1..20).each do |i|
+#   name = Faker::Book.title
+#   author = Faker::Book.author
+#   description = "出版: " + Faker::Book.publisher + ", ジャンル: " + Faker::Book.genre
+#   price = i * 100
+#   num = i
+#   Product.create!(name: name, author: author, description: description, price: price, num: num)
+# end
 
+User.create!(
+  name: "管理ユーザー",
+  email: ENV['ADMIN_EMAIL'],
+  idname: ENV['ADMIN_IDNAME'],
+  password: ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD'],
+  admin: true
+)
 
-(1..20).each do |i|
-  name = Faker::Book.title
-  author = Faker::Book.author
-  description = "出版: " + Faker::Book.publisher + ", ジャンル: " + Faker::Book.genre
-  price = i * 100
-  num = i
-  Product.create!(name: name, author: author, description: description, price: price, num: num)
-end
-
-puts "終了！！"
+puts "---終了---"
