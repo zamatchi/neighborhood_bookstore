7263e84ace79f6019fe08b339d2a8d35c87aaa77
Jon <chipdale0119@gmail.com>
2020-03-15 09:46:18 +0000

    order home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,11 +6,11 @@ class HomesController < ApplicationController
 
     if current_user
       if params[:author]
-        @products = Product.where("author LIKE ?", "%#{params[:author]}%")
+        @products = Product.where("author LIKE ?", "%#{params[:author]}%").page(params[:page]).per(30).order('created_at DESC')
       elsif params[:name]
-        @products = Product.where("name LIKE ?", "%#{params[:name]}%")
+        @products = Product.where("name LIKE ?", "%#{params[:name]}%").page(params[:page]).per(30).order('created_at DESC')
       else
-        @products = Product.all
+        @products = Product.all.page(params[:page]).per(30).order('created_at DESC')
       end
     else
       redirect_to new_user_session_path
